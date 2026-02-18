8a6b6bc5138ab7a005d103ec495791cd35a2a8be
xungwoo <xungwoo@gmail.com>
2026-02-18 23:55:53 +0900

    feat: db connection 방식 변경

--- internal/database/postgres.go
+++ internal/database/postgres.go
@@ -5,23 +5,27 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
-	"github.com/xungwoo/stareps/ent"
 	_ "github.com/lib/pq"
+	"github.com/xungwoo/stareps/ent"
 )
 
 var Client *ent.Client
 
 func Connect() error {
-	dsn := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_SSLMODE"),
-	)
+	dsn := strings.TrimSpace(os.Getenv("DATABASE_URL"))
+	if dsn == "" {
+		dsn = fmt.Sprintf(
+			"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+			os.Getenv("DB_HOST"),
+			os.Getenv("DB_PORT"),
+			os.Getenv("DB_USER"),
+			os.Getenv("DB_PASSWORD"),
+			os.Getenv("DB_NAME"),
+			os.Getenv("DB_SSLMODE"),
+		)
+	}
 
 	client, err := ent.Open("postgres", dsn)
 	if err != nil {
